f7a94342df32ac5731151df02aecc71b4e858970
B-Stanley-0316 <brittneyrstanley07@hotmail.com>
2022-06-18 16:59:08 -0400

    Addind Deliverable 2.

--- Resources/MechaCarChallenge.R
+++ Resources/MechaCarChallenge.R
@@ -2,7 +2,7 @@ library(dplr)
 
 # Linear Regression to Predict MPG
 # Read CSV to dataframe
-mechacar_mpg_df = read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+mechacar_mpg_df <-  read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 head(mechacar_mpg_df)
 
 # linear regression
@@ -10,3 +10,11 @@ lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = 
 #find p-value and r-squared
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df))
 
+# Suspension Coil Lot Analysis
+# Read CSV to dataframe
+suspension_coil_df <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(suspension_coil_df)
+# Create Summary Dataframe
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+#Create Summary for Each Lot
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
